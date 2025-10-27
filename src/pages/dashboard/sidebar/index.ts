c1d9d3bddf3d9baf467060055f8246e0594b6a6d
Bui Huy Hoang <hoangbui03921@S3P.COM>
2025-10-27 09:24:39 +0700

    1.0.15

---
+++ src/pages/dashboard/sidebar/index.ts
@@ -0,0 +1,8 @@
+export { SidebarLeftPart } from "./sidebar-left.part";
+export * from "./sidebar-menu";
+export {
+  DASHBOARD_HR_SIDEBAR,
+  DASHBOARD_HRM_SIDEBAR,
+  DASHBOARD_TREE_SYSTEM_SIDEBAR,
+} from "./sidebar.constant";
+export { SidebarPart } from "./sidebar.part";
