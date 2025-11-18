7c64f09eee63e69d6822a1d3a2700808e1a49bc9
dev1-gmail2025 <sevago.dev1@gmail.com>
2025-11-18 15:13:53 +0700

    1.0.65

--- src/components/styles/image.style.tsx
+++ src/components/styles/image.style.tsx
@@ -2,24 +2,24 @@ import { Box, styled } from '@mui/material';
 import { STYLE } from '../../common/constant';
 import { ImageElement } from '../elements/image/image.element';
 
-// export const ImageBrand = styled(ImageElement)(({ theme }) => ({
-//   height: STYLE.HEIGHT_ELEMENT_OTHER,
-//   border: `1px solid ${theme.palette.divider}`,
-//   borderRadius: STYLE.BORDER_RADIUS_ELEMENT,
-//   '&:hover': { boxShadow: theme.shadows[1] },
-//   padding: 8,
-//   backgroundColor: 'white',
-//   cursor: 'pointer',
-//   width: 100,
-//   objectFit: 'contain',
-// }));
+export const ImageBrand = styled(ImageElement)(({ theme }) => ({
+  height: STYLE.HEIGHT_ELEMENT_OTHER,
+  border: `1px solid ${theme.palette.divider}`,
+  borderRadius: STYLE.BORDER_RADIUS_ELEMENT,
+  '&:hover': { boxShadow: theme.shadows[1] },
+  padding: 8,
+  backgroundColor: 'white',
+  cursor: 'pointer',
+  width: 100,
+  objectFit: 'contain',
+}));
 
-// export const ImageBorderDashboard = styled(ImageElement)(({ theme }) => ({
-//   border: `1px solid ${theme.palette.divider}`,
-//   borderRadius: STYLE.BORDER_RADIUS_ELEMENT,
-//   height: 50,
-//   width: 70,
-// }));
+export const ImageBorderDashboard = styled(ImageElement)(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  borderRadius: STYLE.BORDER_RADIUS_ELEMENT,
+  height: 50,
+  width: 70,
+}));
 
 export const ImageEmpty = styled(Box)(({ theme }) => ({
   width: 'auto',
