7d4e9e195e17e57554e4ea5087cf96fcbf84226c
dev1-gmail2025 <sevago.dev1@gmail.com>
2025-11-19 16:16:22 +0700

    feat: config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import dts from "vite-plugin-dts";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig(({ mode }) => {
-  const isLibrary = mode === "library";
+  const isLibrary = mode === 'library';
 
   if (isLibrary) {
     return {
@@ -12,43 +12,61 @@ export default defineConfig(({ mode }) => {
         react(),
         dts({
           insertTypesEntry: true, // Thêm field "types" vào package.json
-          outDir: "dist", // Xuất file .d.ts vào thư mục dist
+          outDir: 'dist', // Xuất file .d.ts vào thư mục dist
         }),
       ],
       build: {
         lib: {
-          entry: resolve(__dirname, "src/index.ts"),
-          name: "SevagoSSO",
-          fileName: (format) => `index.${format === "es" ? "esm" : format}.js`,
-          formats: ["es", "cjs"],
+          entry: resolve(__dirname, 'src/index.ts'),
+          name: 'SevagoSSO',
+          fileName: (format) => `index.${format === 'es' ? 'esm' : format}.js`,
+          formats: ['es', 'cjs'],
         },
         rollupOptions: {
           external: [
-            "react",
-            "react-dom",
-            "react/jsx-runtime", // Bổ sung thêm react/jsx-runtime để tránh bundle React runtime
-            "@emotion/react",
-            "@emotion/styled",
-            "@mui/material",
-            "@mui/system",
-            "@reduxjs/toolkit",
-            "react-redux",
-            "react-router-dom",
-            "redux-persist",
+            //đảm bảo mọi peer (react, react-dom, formik, v.v.) không được bundle vào dist.
+            'react',
+            'react-dom',
+            'react/jsx-runtime', // Bổ sung thêm react/jsx-runtime để tránh bundle React runtime
+            '@emotion/react',
+            '@emotion/styled',
+            '@mui/material',
+            '@mui/system',
+            '@reduxjs/toolkit',
+            'react-redux',
+            'react-router-dom',
+            'redux-persist',
+            'formik',
+            'yup',
+            'dayjs',
+            'framer-motion',
+            'axios',
+            'socket.io-client',
+            '@mui/x-date-pickers',
+            'react-number-format',
+            '@types/react-router-dom',
           ],
           output: {
             globals: {
-              react: "React",
-              "react-dom": "ReactDOM",
-              "react/jsx-runtime": "jsxRuntime",
-              "@emotion/react": "EmotionReact",
-              "@emotion/styled": "EmotionStyled",
-              "@mui/material": "MaterialUI",
-              "@mui/system": "MaterialUISystem",
-              "@reduxjs/toolkit": "ReduxToolkit",
-              "react-redux": "ReactRedux",
-              "react-router-dom": "ReactRouterDOM",
-              "redux-persist": "ReduxPersist",
+              react: 'React',
+              'react-dom': 'ReactDOM',
+              'react/jsx-runtime': 'jsxRuntime',
+              '@emotion/react': 'EmotionReact',
+              '@emotion/styled': 'EmotionStyled',
+              '@mui/material': 'MaterialUI',
+              '@mui/system': 'MaterialUISystem',
+              '@reduxjs/toolkit': 'ReduxToolkit',
+              'react-redux': 'ReactRedux',
+              'react-router-dom': 'ReactRouterDOM',
+              'redux-persist': 'ReduxPersist',
+              formik: 'Formik',
+              yup: 'Yup',
+              dayjs: 'dayjs',
+              'framer-motion': 'FramerMotion',
+              axios: 'axios',
+              'socket.io-client': 'io',
+              '@mui/x-date-pickers': 'MuiDatePickers',
+              'react-number-format': 'ReactNumberFormat',
             },
           },
         },
